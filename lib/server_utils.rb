7e4c718e36cf917b64e84d407013cbecfeaee8da
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2017-01-03 15:29:42 -0500

    Updated Gemfile. Created 'ServerUtils' module. Began scaffolding the '/trends' route.

---
+++ lib/server_utils.rb
@@ -0,0 +1,19 @@
+module ServerUtils
+
+	def self.get_country_id(countryList)
+		puts countryList.split(",")
+
+
+		#return array with Google Trends country ids from the countryList
+		countryTable = { "United States" => "p1",
+										"United Kingdom" => "p9",
+										"Canada" => "p13",
+										"France" => "p16",
+										"Germany" => "p15",
+										"Japan" => "p4",
+										"Mexico" => "p21" }
+
+		puts countryTable[countryList]
+	end
+
+end
--- server.rb
+++ server.rb
@@ -1,7 +1,25 @@
+
+require 'net/http'
 require 'sinatra'
+require 'sinatra/json'
+require './lib/server_utils'
+
 
 set :bind, '0.0.0.0'
 
 get '/' do 
-	"Hello World!"
+	json :foo => 'bar'
+end
+
+get '/trends' do 
+
+	ServerUtils::get_country_id(params[:countries])
+
+	# url = URI.parse("http://www.google.com/trends/hottrends/atom/feed?pn=#{ServerUtils::get_country_id(params[:countries])}")
+	# 
+	url = URI.parse("http://www.google.com/trends/hottrends/atom/feed?pn=p9")
+
+	content_type 'text/xml'
+	response = Net::HTTP.get_response(url)
+	response.body
 end
