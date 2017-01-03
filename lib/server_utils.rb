f5ca38b3a7eb36e1f8f8950481b9ee6f07cc6a0f
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2017-01-03 15:56:09 -0500

    Style update.

--- lib/server_utils.rb
+++ lib/server_utils.rb
@@ -4,13 +4,15 @@ module ServerUtils
 		puts countryList.split(",")
 
 		#return array with Google Trends country ids from the countryList
-		countryTable = { "United States" => "p1",
-										"United Kingdom" => "p9",
-										"Canada" => "p13",
-										"France" => "p16",
-										"Germany" => "p15",
-										"Japan" => "p4",
-										"Mexico" => "p21" }
+		countryTable = { 
+			"United States" => "p1",
+			"United Kingdom" => "p9",
+			"Canada" => "p13",
+			"France" => "p16",
+			"Germany" => "p15",
+			"Japan" => "p4",
+			"Mexico" => "p21", 
+		}
 
 		puts countryTable[countryList]
 	end
