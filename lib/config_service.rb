60920f222fd3d2c134522923ac7e0d9d82908aa6
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2017-01-07 16:34:58 -0500

    Renamed 'config.json' file in 'config_service.rb'

--- lib/config_service.rb
+++ lib/config_service.rb
@@ -2,7 +2,7 @@ module AppConfig
   require "json"
 
   def self.get_config_var(variable)
-    config_vars = JSON.parse(File.read("./config/config.json"))
+    config_vars = JSON.parse(File.read("./config/auth.json"))
     config_vars[variable]
   end
 
