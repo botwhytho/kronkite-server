b181fd4ba55b2aef459c2ff08d84396b2a8d152c
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2017-03-02 12:48:09 -0500

    Added 'open-uri' gem to 'FeedService' module. Created 'fetch_feed_article' to return parsed HTML from URLs sent as parameters to '/article' route.

--- lib/feed_service.rb
+++ lib/feed_service.rb
@@ -1,14 +1,25 @@
 module FeedService
   require "./lib/auth_service"
   require "net/http"
-  require "crack/xml"
+  require "open-uri"
   require "json"
   require "pp"
 
   API_URL = AuthService.get_auth("googleTrendsAPIURL")
+  MERCURY_API_KEY = AuthService.get_auth("mercuryAPI")["APIKey"]
+  MERCURY_API_URL = AuthService.get_auth("mercuryAPI")["URL"]
      
   def self.fetch_feed()
     Net::HTTP.get_response(URI.parse(API_URL)).body
+  end
+
+  def self.fetch_feed_article(params)
+  	headers = {"x-api-key" => MERCURY_API_KEY }
+
+  	request = open("#{MERCURY_API_URL}#{params}",
+  		{"x-api-key" => MERCURY_API_KEY })
+
+ 	request.read
   end
 
 end
--- server.rb
+++ server.rb
@@ -1,4 +1,5 @@
 
+require "pp"
 require "sinatra"
 require "sinatra/json"
 require "./lib/feed_service"
@@ -7,11 +8,21 @@ require "./lib/music_service"
 
 set :bind, "0.0.0.0"
 
+before do
+   response["Access-Control-Allow-Origin"] = "http://localhost:8888"
+ 
+end
+
+options "*" do
+	headers "Access-Control-Allow-Headers" => "Content-Type",
+    		"Access-Control-Allow-Methods" => "POST"
+end
+
 get "/" do 
   json :foo => "bar"
 end
 
-get "/trends" do 
+get "/trending-search" do
   content_type("application/json")
   FeedService.fetch_feed()
 end
@@ -26,3 +37,7 @@ get "/music" do
   json(MusicService.get_tracks)
 end
 
+get "/article" do 
+  content_type("application/json")
+  FeedService.fetch_feed_article(params["url"])
+end
