1f5ea1e1a8b0d873489067bb65bb40480aecf915
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2017-03-31 17:41:42 -0400

    Finished scaffolding #MusicService.get_tracks method. Track data from the Spotify API now returns in a JSON response.

--- lib/music_service.rb
+++ lib/music_service.rb
@@ -8,8 +8,22 @@ module MusicService
     RSpotify.authenticate(ENV["SPOTIFY_API_CLIENTID"], 
       ENV["SPOTIFY_API_CLIENT_SECRET"])
     category = RSpotify::Category.find("toplists", country: "US")
+    tracks = category.playlists.first.tracks.first(10)
+    parsed_track_data = []
 
-    pp category.playlists.first.tracks
+    tracks.each { |track|  
+        track_data = {popularity: track.popularity,
+                      name: track.name,
+                      artists: track.artists.map{|artist| artist.name},
+                      album: track.album.name,
+                      album_art: track.album.images,
+                      release_date: track.album.release_date,
+                      preview_url: track.preview_url}
+
+        parsed_track_data.push(track_data)
+    }
+    pp parsed_track_data.push({uri: category.playlists.first.uri})
+
   end
 
 end
