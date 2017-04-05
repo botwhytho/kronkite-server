45e4a416d8e3df65b7396c6ccc684212cbbf2a87
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2017-04-05 13:55:43 -0400

    Re-wired 'MusicService' module to live API data. Sorted API response by track popularity.

--- lib/music_service.rb
+++ lib/music_service.rb
@@ -8,26 +8,28 @@ module MusicService
   def self.get_tracks()
 
 
-    tracks = JSON.parse(File.read("./lib/sample-api-data.json"))
+    #tracks = JSON.parse(File.read("./lib/sample-api-data.json"))
 
-    #RSpotify.authenticate(ENV["SPOTIFY_API_CLIENTID"], 
-    #  ENV["SPOTIFY_API_CLIENT_SECRET"])
-    #category = RSpotify::Category.find("toplists", country: "US")
-    #tracks = category.playlists.first.tracks.first(10)
-    #parsed_track_data = []
+    RSpotify.authenticate(ENV["SPOTIFY_API_CLIENTID"], 
+      ENV["SPOTIFY_API_CLIENT_SECRET"])
+    category = RSpotify::Category.find("toplists", country: "US")
+    tracks = category.playlists.first.tracks.first(25)
+    parsed_track_data = []
 
-    #tracks.each { |track|  
-      #   track_data = {popularity: track.popularity,
-      #                name: track.name,
-      #               artists: track.artists.map{|artist| artist.name},
-      #              album: track.album.name,
-      #                album_art: track.album.images,
-      #                release_date: track.album.release_date,
-      #                preview_url: track.preview_url}
-      #  parsed_track_data.push(track_data)
-    #}
-    #pp parsed_track_data.push({uri: category.playlists.first.uri})
-    return tracks
+    tracks.each { |track|  
+        track_data = {popularity: track.popularity,
+                      name: track.name,
+                      artists: track.artists.map{|artist| artist.name},
+                      album: track.album.name,
+                      album_art: track.album.images,
+                      release_date: track.album.release_date,
+                      preview_url: track.preview_url}
+        parsed_track_data.push(track_data)
+    }
+
+    parsed_track_data.select! { |track| track[:popularity] > 80 } 
+    parsed_track_data.sort_by! { |track| track[:popularity] }
+    parsed_track_data.reverse
   end
 
 end
